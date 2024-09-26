a3062d8f9308399cccb1995ed6cdae642b88cf1c
Drupal <143964302+drupalgrupal@users.noreply.github.com>
2024-09-26 12:20:54 -0400

    Update Get or Find SQL Databases.ps1
    Minor changes to the PowerShell script

--- Get or Find SQL Databases.ps1
+++ Get or Find SQL Databases.ps1
@@ -1,85 +1,78 @@
 # Related blog post:  https://sqlpal.blogspot.com/2019/07/powershell-script-to-get-list-of.html
 
-
 try
 {
+    Import-Module -Name SqlServer -ErrorAction Stop
 
-        Import-Module -Name SqlServer
+    $server_name   = "Server1" # SERVER/HOST NAME HERE  
+    $database_name = "admin"   # NAME OF THE DATABASE YOU WOULD LIKE TO SEARCH OTHERWISE LEAVE THIS BLANK
+    $exact_match   = "N"       # WHETHER TO SEARCH FOR AN EXACT DATABASE NAME
 
-        $server_name   = "Server1"    # SERVER/HOST NAME HERE
-        $database_name = "admin"      # NAME OF THE DATABASE YOU WOULD LIKE TO SEARCH OTHERWISE LEAVE THIS BLANK
-        $exact_match   = "N"          # WHETHER TO SEARCH FOR AN EXACT DATABASE NAME
+    $logfile = "$env:TEMP\logfile_" + (Get-Date).toString("yyyyMMdd_HHmmss") + ".txt"
+    
+    "Start Time: " + (Get-Date)  | Out-File -Append $logFile
+    "Log: $logfile"  | Out-File -Append $logFile
+    ""  | Out-File -Append $logFile
+    "Server: $server_name" | Out-File -Append $logFile
+    "Database: $database_name" | Out-File -Append $logFile
+    "Exact Match: $exact_match" | Out-File -Append $logFile
+    ""  | Out-File -Append $logFile
 
 
-        # if $server_name string is empty or null then use the current computer name
-        if ($server_name -eq "" -or $server_name -eq $null)
+    if ($server_name -eq "" -or $server_name -eq $null)
+    {
+        $server_name = $env:computername
+    }
+
+    $sql_services = Get-WmiObject -Query "select * from win32_service where PathName like '%%sqlservr.exe%%'" -ComputerName "$server_name" -ErrorAction Stop
+
+    foreach ($sql_service in $sql_services) 
+    {
+        $instance_name = $sql_service.Name -replace "MSSQL\$", ""
+        if ($sql_service.State -eq "Running")
         {
-                $server_name = $env:computername
-        }
+            $sql_connection = if ($instance_name -eq "MSSQLSERVER") { $sql_service.PSComputerName } else { $sql_service.PSComputerName + "\" + $instance_name }
 
-        "Looking up SQL Services on: $server_name"
-        $sql_services = Get-WmiObject -Query "select * from win32_service
-        where PathName like '%%sqlservr.exe%%'"  -ComputerName "$server_name" -ErrorAction Stop
-        
+            if ($database_name -eq "")
+            {
+                Get-SqlDatabase -ServerInstance $sql_connection | 
+                    FT Parent, Name, Owner, ReadOnly, RecoveryModel, Size, Status, UserAccess
+            }
+            else
+            {
 
-        if ($sql_services.PSComputerName -ne $null)
-        {
-                # Display sql services on the screen
-
-                $sql_services  |  select-object -Property PSComputerName, @{n="sql_instance";e={$_.Name -replace "MSSQL\$", ""}}, Name, ProcessID, StartMode, State, Status, ExitCode, PathName | ft -AutoSize
-
-
-                foreach ($sql_service in $sql_services) 
+                if ($exact_match -eq "Y")
                 {
-
-                        ""
-                        $instnace_name = $sql_service.Name -replace "MSSQL\$", ""
-                        "Instnace Name: " + $instnace_name
-
-                        if ($instnace_name -eq "MSSQLSERVER") 
-                
-                        {
-                                $sql_connection = $sql_service.PSComputerName
-                        }
-
-                        else 
-                        {
-                                $sql_connection = $sql_service.PSComputerName + "\" + $instnace_name
-                        }
-
-                        $sql_connection + ": " + $sql_service.State
-
-                        if ($sql_service.State -eq "Running")
-                        {
-                                if ($database_name -eq "")
-                                {
-                                        Get-SqlDatabase -ServerInstance $sql_connection 
-                                }
-                                else
-                                {
-                                        if ($exact_match -eq "Y")
-                                        {
-                                                Get-SqlDatabase -ServerInstance $sql_connection | where {$_.name -eq $database_name} |  ft -AutoSize
-                                        }
-                                        else
-                                        {
-                                                Get-SqlDatabase -ServerInstance $sql_connection | where {$_.name -like "*$database_name*"} | ft -AutoSize
-                                        }
-                                }
-                        }
-                        else
-                        {
-                                "Skipping $sql_connection as its not running..."
-                        }
-
+                    Get-SqlDatabase -ServerInstance $sql_connection | Where-Object {$_.name -eq $database_name} | 
+                        FT Parent, Name, Owner, ReadOnly, RecoveryModel, Size, Status, UserAccess
                 }
-        }
-}
+                else
+                {
+                    Get-SqlDatabase -ServerInstance $sql_connection | Where-Object {$_.name -like "*$database_name*"} | 
+                        FT Parent, Name, Owner, ReadOnly, RecoveryModel, Size, Status, UserAccess
+                }
+            }
 
+
+        }
+        else
+        {
+            "Skipping $sql_connection as it's not running..." | Out-File -Append $logFile
+        }
+    }
+
+    ""  | Out-File -Append $logFile
+    "Completion Time: " + (Get-Date)  | Out-File -Append $logFile
+  
+}
 Catch
 {
-        (Get-Date).ToString() + ": Error Occurred" 
-        throw $_  
-        return
+    $errorMessage = (Get-Date).ToString() + ": Error Occurred - " + $_.Exception.Message
+    $errorMessage | Out-File -Append $logFile
+    throw
 }
+
+"# Launch the notepad.exe to view the log file"
+"notepad.exe $logfile"
+
 
